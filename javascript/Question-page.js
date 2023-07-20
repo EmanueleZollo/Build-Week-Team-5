486a82ab95238d8caae4d3350c1972bcb48467b4
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-07-20 14:52:35 +0200

    Donut chart call after svuota function

--- javascript/Question-page.js
+++ javascript/Question-page.js
@@ -554,7 +554,7 @@ function showQuestion() {
   let answers = currentQuestion.incorrect_answers.concat(currentQuestion.correct_answer);
   randomQuestions(answers);
 
-  answers.forEach((answer) => {
+  answers.forEach(answer => {
     const button = document.createElement("button");
     button.innerHTML = answer;
     button.classList.add("answer");
@@ -581,7 +581,7 @@ function selectAnswer(event) {
   nextButton.style.display = "inline-block";
 
   const answerButtons = document.querySelectorAll(".btn");
-  answerButtons.forEach((button) => (button.disabled = true));
+  answerButtons.forEach(button => (button.disabled = true));
   console.log(score);
 }
 
@@ -609,34 +609,36 @@ function correct() {
 
 function svuota() {
   document.getElementById("Svuota").innerHTML = `
-  <main>
-    <header>
-      <img class="logo" src="./assets/epicode_logo.png" alt="logo-EPICODE" />
-    </header>
-    <div class="results-title-container">
-      <h2>Results</h2>
-      <h4>The summary of your answers:</h4>
-    </div>
-    <section class="results-section-container">
-      <div class="results-left-container">
-        <h2 class="correct-wrong" id="resultScore">Correct<br /><strong class="percentage">%</strong></h2>
-        <p class="right-answers"></p>
+<body>
+    <main>
+      <header>
+        <img class="logo" src="./assets/epicode_logo.png" alt="logo-EPICODE" />
+      </header>
+      <div class="results-title-container">
+        <h2>Results</h2>
+        <h4>The summary of your answers:</h4>
       </div>
-      <div class="results-central-container">
-        <h5>Congratulations!<span>You passed the exam.</span></h5>
-        <p>We'll send you the certificate in a few minutes. Check your email (including promotions / spam folder)</p>
-      </div>
-      <div class="results-right-container">
-        <h2 class="correct-wrong" id="wrong">Wrong<br /><strong class="percentage">%</strong></h2>
-        <p class="wrong-answers"></p>
-      </div>
-    </section>
-    <footer class="results-footer">
-      <button class="results-button"><a>RATE US</a></button>
-    </footer>
-  </main>
+      <section class="results-section-container">
+        <div class="results-left-container">
+          <h2 class="correct-wrong" id="resultScore">Correct<br /><strong class="percentage">%</strong></h2>
+          <p class="right-answers"></p>
+        </div>
+        <div class="results-central-container">
+        <canvas id="myDonutChart" width="600" height="600"></canvas>
+        </div>
+        <div class="results-right-container">
+          <h2 class="correct-wrong" id="wrong">Wrong<br /><strong class="percentage">%</strong></h2>
+          <p class="wrong-answers"></p>
+        </div>
+      </section>
+      <footer class="results-footer">
+        <button class="results-button"><a>RATE US</a></button>
+      </footer>
+    </main>
+</body>
 `;
   correct();
+  drawChart();
 }
 
 function randomQuestions(array) {
@@ -696,7 +698,80 @@ function onTimesUp() {
 
 // RESULT PAGE
 
-localStorage.setItem("scoreX", scoreX);
+const drawChart = function () {
+  const corrette = 60;
+  const sbagliate = 40;
 
-/*const resScore = document.getElementById("resultScore");
-resScore.innerText = scoreX;*/
+  const coloreCorrette = "cyan";
+  const coloreSbagliate = "MediumVioletRed";
+
+  const canvas = document.getElementById("myDonutChart");
+  const ctx = canvas.getContext("2d");
+
+  const canvasWidth = canvas.width;
+  const canvasHeight = canvas.height;
+
+  const radius = Math.min(canvasWidth, canvasHeight) / 2.3;
+
+  const centerX = canvasWidth / 2;
+  const centerY = canvasHeight / 2;
+
+  function drawDonutArc(startAngle, endAngle, colore) {
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+    ctx.arc(centerX, centerY, radius * 0.7, endAngle, startAngle, true);
+    ctx.closePath();
+    ctx.fillStyle = colore;
+    ctx.fill();
+  }
+
+  function drawText() {
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    const fontSize = 25;
+    ctx.font = `${fontSize}px Inter`;
+
+    if (corrette >= 60) {
+      ctx.fillStyle = "white";
+      ctx.font = `bold ${fontSize}px Inter`;
+      ctx.fillText("Congratulations!", centerX, centerY - fontSize * 2);
+      ctx.fillStyle = "cyan";
+      ctx.fillText("You passed the exam.", centerX, centerY - fontSize);
+      ctx.fillStyle = "white";
+      ctx.font = `18px Inter`;
+      ctx.fillText("We'll send you the certificate", centerX, centerY + fontSize * 0.75);
+      ctx.fillText("in a few minutes.", centerX, centerY + fontSize * 1.5);
+      ctx.fillText("Check your email (including", centerX, centerY + fontSize * 2.25);
+      ctx.fillText("promotions / spam folder)", centerX, centerY + fontSize * 3);
+    } else {
+      ctx.font = `bold ${fontSize}px Inter`;
+      ctx.fillStyle = "red";
+      ctx.fillText("You failed the exam.", centerX, centerY - fontSize);
+      ctx.fillStyle = "white";
+      ctx.font = `18px Inter`;
+      ctx.fillText("But don't worry,", centerX, centerY + fontSize * 0.75);
+      ctx.fillText("you can always retry", centerX, centerY + fontSize * 1.5);
+    }
+  }
+
+  const total = corrette + sbagliate;
+  const corretteAngle = (corrette / total) * 2 * Math.PI;
+  const sbagliateAngle = (sbagliate / total) * 2 * Math.PI;
+  drawDonutArc(-Math.PI / 2 - corretteAngle, -Math.PI / 2, coloreCorrette);
+
+  drawDonutArc(-Math.PI / 2, -Math.PI / 2 + sbagliateAngle, coloreSbagliate);
+
+  drawText();
+};
+
+const websiteButton = document.querySelector(".results-button");
+const newLink = document.querySelector("a");
+
+const feedbackLink = function () {
+  newLink.classList.add("linky");
+  newLink.setAttribute("href", "./Feedback-page.html");
+  return websiteButton.appendChild(newLink);
+};
+
+websiteButton.addEventListener("click", feedbackLink);
